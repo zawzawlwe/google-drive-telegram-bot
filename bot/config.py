d26586bc26898cd159e81715364c0bf548e7edbc
zawzawlwe <86354314+zawzawlwe@users.noreply.github.com>
2021-08-13 17:01:53 +0630

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,13 +1,13 @@
 class config:
-    BOT_TOKEN = ""
-    APP_ID = ""
-    API_HASH = ""
+    BOT_TOKEN = "1806399347:AAGICGhO4V6hAlagfoyJbgoTEsHsB5mtTPA"
+    APP_ID = "6660481"
+    API_HASH = "874c349be97bbcd1ad6a3562589b5d3d"
     DATABASE_URL = ""
     SUDO_USERS = "" # Sepearted by space.
     SUPPORT_CHAT_LINK = ""
     DOWNLOAD_DIRECTORY = "./downloads/"
-    G_DRIVE_CLIENT_ID = ""
-    G_DRIVE_CLIENT_SECRET = ""
+    G_DRIVE_CLIENT_ID = "867042696756-nrcbgp49d05jkbs8gv52vkjtgt83t9uh.apps.googleusercontent.com"
+    G_DRIVE_CLIENT_SECRET = "1SsOZwshI_w_iM35UL6wC97P"
 
 
 class BotCommands:
